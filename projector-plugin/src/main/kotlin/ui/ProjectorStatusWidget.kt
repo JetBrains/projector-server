889b58f9d9c10a99fbc881fef7acb9881944cd80
Ilya Krukov <ikrukov@horis.ru>
2021-05-26 14:38:01 +0300

    Return to SVG icons

--- projector-plugin/src/main/kotlin/ui/ProjectorStatusWidget.kt
+++ projector-plugin/src/main/kotlin/ui/ProjectorStatusWidget.kt
@@ -29,7 +29,7 @@ import com.intellij.ide.DataManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.popup.JBPopupFactory
 import com.intellij.openapi.ui.popup.ListPopup
-
+import com.intellij.openapi.util.IconLoader
 import com.intellij.openapi.wm.StatusBar
 import com.intellij.openapi.wm.StatusBarWidget
 import com.intellij.openapi.wm.StatusBarWidget.WidgetPresentation
@@ -42,12 +42,7 @@ import isProjectorDisabled
 import isProjectorRunning
 import java.awt.Component
 import java.awt.event.MouseEvent
-import java.awt.image.BufferedImage
 import javax.swing.Icon
-import javax.swing.ImageIcon
-import java.awt.Graphics
-import java.awt.Color
-import java.awt.Image
 
 
 class ProjectorStatusWidget(project: Project)
@@ -120,30 +115,9 @@ class ProjectorStatusWidget(project: Project)
 
   private companion object {
     private const val PROJECTOR_TOOLBAR_ACTION_GROUP = "projector.toolbar"
-    private val RED_DOT: Icon by lazy { createIcon(Color.RED) }
-    private val GREEN_DOT: Icon by lazy { createIcon(Color.GREEN) }
-    private val YELLOW_DOT: Icon by lazy { createIcon(Color.YELLOW) }
-    private val BLACK_DOT: Icon by lazy { createIcon(Color.BLACK) }
-
-    private const val ICON_SIZE = 16
-    private const val DOT_RADIUS = 3
-    private const val SCALE_FACTOR = 4
-
-    private fun createScaledImage(color: Color): BufferedImage {
-      val scaledImage = BufferedImage(SCALE_FACTOR * ICON_SIZE, SCALE_FACTOR * ICON_SIZE, BufferedImage.TYPE_INT_ARGB)
-      val g: Graphics = scaledImage.graphics
-      g.color = color
-      val shift = SCALE_FACTOR * (ICON_SIZE.div(2) - DOT_RADIUS)
-      g.fillOval(shift, shift, SCALE_FACTOR * DOT_RADIUS * 2, SCALE_FACTOR * DOT_RADIUS * 2)
-      g.dispose()
-
-      return scaledImage
-    }
-
-    private fun createIcon(color: Color): Icon {
-      val scaledImage = createScaledImage(color)
-      val image = scaledImage.getScaledInstance(ICON_SIZE, ICON_SIZE, Image.SCALE_SMOOTH)
-      return ImageIcon(image)
-    }
+    private val RED_DOT: Icon by lazy { IconLoader.getIcon("/META-INF/redSign.svg", ProjectorStatusWidget::class.java) }
+    private val GREEN_DOT: Icon by lazy { IconLoader.getIcon("/META-INF/greenSign.svg", ProjectorStatusWidget::class.java) }
+    private val YELLOW_DOT: Icon by lazy { IconLoader.getIcon("/META-INF/yellowSign.svg", ProjectorStatusWidget::class.java) }
+    private val BLACK_DOT: Icon by lazy { IconLoader.getIcon("/META-INF/blackSign.svg", ProjectorStatusWidget::class.java) }
   }
 }
